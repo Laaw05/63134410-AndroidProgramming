8ee9792c655ef1332c132c3ee4495ad96ebef11f
Laaw05 <dhoanglam79@gmail.com>
2024-03-24 22:56:53 +0700

    AlarmReceiver1

---
+++ Cau2_AlarmClock/app/src/main/java/ntu/dhl63134410/cau2_alarmclock/AlarmReceiver.java
@@ -0,0 +1,21 @@
+package ntu.dhl63134410.cau2_alarmclock;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent nextActivity= new Intent(context, NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                nextActivity,
+                PendingIntent.FLAG_IMMUTABLE // hoặc PendingIntent.FLAG_MUTABLE
+        );
+    }
+}

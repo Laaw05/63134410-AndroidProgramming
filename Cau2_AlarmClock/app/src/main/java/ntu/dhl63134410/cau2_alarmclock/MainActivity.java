30cef3d93bcd235bf466eb0cb80c5df206e14c07
Laaw05 <dhoanglam79@gmail.com>
2024-03-24 23:13:33 +0700

    khai bao bien

--- Cau2_AlarmClock/app/src/main/java/ntu/dhl63134410/cau2_alarmclock/MainActivity.java
+++ Cau2_AlarmClock/app/src/main/java/ntu/dhl63134410/cau2_alarmclock/MainActivity.java
@@ -2,9 +2,23 @@ package ntu.dhl63134410.cau2_alarmclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.os.Bundle;
 
+import com.google.android.material.timepicker.MaterialTimePicker;
+
+import java.util.Calendar;
+
+import ntu.dhl63134410.cau2_alarmclock.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MaterialTimePicker timePicker;
+    private Calendar calendar;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

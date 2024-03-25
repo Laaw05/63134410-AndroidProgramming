262483c4bee1ca260f77b4c31c047944ce38136d
Laaw05 <dhoanglam79@gmail.com>
2024-03-25 12:04:51 +0700

    add link

--- Cau3_Profile/app/src/main/java/ntu/dhl63134410/cau3_profile/MainActivity.java
+++ Cau3_Profile/app/src/main/java/ntu/dhl63134410/cau3_profile/MainActivity.java
@@ -2,7 +2,11 @@ package ntu.dhl63134410.cau3_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView fblinked =(TextView) findViewById(R.id.facebook);
+        TextView youtubelinked= (TextView) findViewById(R.id.youtube);
+        fblinked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.facebook.com/dhlam79");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        youtubelinked.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.youtube.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }

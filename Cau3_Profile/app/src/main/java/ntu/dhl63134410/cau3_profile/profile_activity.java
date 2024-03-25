765bd42bb99aec4de918300f3dbc504cb05452a3
Laaw05 <dhoanglam79@gmail.com>
2024-03-25 10:41:53 +0700

    them anh

---
+++ Cau3_Profile/app/src/main/java/ntu/dhl63134410/cau3_profile/profile_activity.java
@@ -0,0 +1,100 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		profile
+	 *	@date 		Monday 25th of March 2024 03:32:54 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class profile_activity extends Activity {
+
+	
+	private View _bg__profile_ek2;
+	private View _bg__information_ek1;
+	private View _bg__name_ek1;
+	private TextView name_ek2;
+	private View _bg__info_ek1;
+	private TextView __ng_ho_ng_l_m;
+	private View _bg__username_ek1;
+	private TextView username_ek2;
+	private View _bg__info_ek3;
+	private TextView __hoanglam;
+	private View _bg__email_ek1;
+	private TextView email_ek2;
+	private View _bg__info_ek5;
+	private TextView lam_dh_63cntt_ntu_edu_vn;
+	private View _bg__links_ek1;
+	private TextView links_ek2;
+	private View _bg__urls_ek1;
+	private TextView facebook;
+	private TextView hoang_lam_dang___youtube;
+	private View _bg__bio_ek1;
+	private TextView bio_ek2;
+	private View _bg__info_ek7;
+	private TextView sinh_vi_n_ng_nh_c_ng_ngh__th_ng_tin_n_m_3_hi_n__ang_theo_h_c_t_i_tr__ng___i_h_c_nha_trang__mssv__63134410;
+	private ImageView header;
+	private ImageView vector_1;
+	private ImageView ellipse_1;
+	private ImageView vector_3;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile);
+
+		
+		_bg__profile_ek2 = (View) findViewById(R.id._bg__profile_ek2);
+		_bg__information_ek1 = (View) findViewById(R.id._bg__information_ek1);
+		_bg__name_ek1 = (View) findViewById(R.id._bg__name_ek1);
+		name_ek2 = (TextView) findViewById(R.id.name_ek2);
+		_bg__info_ek1 = (View) findViewById(R.id._bg__info_ek1);
+		__ng_ho_ng_l_m = (TextView) findViewById(R.id.__ng_ho_ng_l_m);
+		_bg__username_ek1 = (View) findViewById(R.id._bg__username_ek1);
+		username_ek2 = (TextView) findViewById(R.id.username_ek2);
+		_bg__info_ek3 = (View) findViewById(R.id._bg__info_ek3);
+		__hoanglam = (TextView) findViewById(R.id.__hoanglam);
+		_bg__email_ek1 = (View) findViewById(R.id._bg__email_ek1);
+		email_ek2 = (TextView) findViewById(R.id.email_ek2);
+		_bg__info_ek5 = (View) findViewById(R.id._bg__info_ek5);
+		lam_dh_63cntt_ntu_edu_vn = (TextView) findViewById(R.id.lam_dh_63cntt_ntu_edu_vn);
+		_bg__links_ek1 = (View) findViewById(R.id._bg__links_ek1);
+		links_ek2 = (TextView) findViewById(R.id.links_ek2);
+		_bg__urls_ek1 = (View) findViewById(R.id._bg__urls_ek1);
+		facebook = (TextView) findViewById(R.id.facebook);
+		hoang_lam_dang___youtube = (TextView) findViewById(R.id.hoang_lam_dang___youtube);
+		_bg__bio_ek1 = (View) findViewById(R.id._bg__bio_ek1);
+		bio_ek2 = (TextView) findViewById(R.id.bio_ek2);
+		_bg__info_ek7 = (View) findViewById(R.id._bg__info_ek7);
+		sinh_vi_n_ng_nh_c_ng_ngh__th_ng_tin_n_m_3_hi_n__ang_theo_h_c_t_i_tr__ng___i_h_c_nha_trang__mssv__63134410 = (TextView) findViewById(R.id.sinh_vi_n_ng_nh_c_ng_ngh__th_ng_tin_n_m_3_hi_n__ang_theo_h_c_t_i_tr__ng___i_h_c_nha_trang__mssv__63134410);
+		header = (ImageView) findViewById(R.id.header);
+		vector_1 = (ImageView) findViewById(R.id.vector_1);
+		ellipse_1 = (ImageView) findViewById(R.id.ellipse_1);
+		vector_3 = (ImageView) findViewById(R.id.vector_3);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	

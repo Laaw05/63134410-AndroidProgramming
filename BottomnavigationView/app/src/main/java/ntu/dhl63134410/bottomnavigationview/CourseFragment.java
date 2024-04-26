c2a775da3403dbf4af6c70fac59dd2d2b559593c
Laaw05 <dhoanglam79@gmail.com>
2024-04-26 15:37:25 +0700

    build(CourseFragment): Thêm recyclerView vào layout Course

--- BottomnavigationView/app/src/main/java/ntu/dhl63134410/bottomnavigationview/CourseFragment.java
+++ BottomnavigationView/app/src/main/java/ntu/dhl63134410/bottomnavigationview/CourseFragment.java
@@ -1,64 +1,58 @@
 package ntu.dhl63134410.bottomnavigationview;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CourseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CourseFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private ArrayList<Integer> courseImg;
+    private ArrayList<String> courseName;
 
     public CourseFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CourseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CourseFragment newInstance(String param1, String param2) {
-        CourseFragment fragment = new CourseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        // Initialize course data
+        courseImg = new ArrayList<>(Arrays.asList(
+                R.drawable.data_structure, R.drawable.c_plus_plus,
+                R.drawable.c_hash, R.drawable.java_script,
+                R.drawable.java, R.drawable.c,
+                R.drawable.html, R.drawable.css));
+
+        courseName = new ArrayList<>(Arrays.asList(
+                "Data Structure", "C++", "C#", "JavaScript", "Java",
+                "C-Language", "HTML 5", "CSS"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course, container, false);
+        View view = inflater.inflate(R.layout.fragment_course, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        // Setting the layout as linear layout for vertical orientation
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Initialize Adapter with course data
+        Adapter adapter = new Adapter(getContext(), courseImg, courseName);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
